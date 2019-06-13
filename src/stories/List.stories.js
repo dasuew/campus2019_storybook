ca050119c486e34456ca926f0093a9f262ec091c
Uwe M <dasuew@hotmail.de>
2019-06-13 03:26:12 +0200

    added test to list.js

--- src/stories/List.stories.js
+++ src/stories/List.stories.js
@@ -3,8 +3,11 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { withInfo } from '@storybook/addon-info'
+import { withTests } from '@storybook/addon-jest'
 import { jsxDecorator } from 'storybook-addon-jsx'
 import { withKnobs, text, boolean, number, array, object } from '@storybook/addon-knobs'
+
+import results from '../../.jest-test-results.json'
 
 import List from '../components/List'
 const items = ['Blubb', 'Bla']
@@ -13,6 +16,8 @@ storiesOf('Campus 2019/List', module)
   .addDecorator(withInfo)
   .addDecorator(withKnobs)
   .addDecorator(jsxDecorator)
+  .addParameters({ jest: ['List'] })
+  .addDecorator(withTests({ results }))
   .add('simple List', () => {
     const itemsKnob = array('Items', items)
     const titleKnob = text('Title', 'Megaliste')
